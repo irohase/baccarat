0fdbd64e9da437b9d85fb412cd923ab628e0f17b
irohase <irohase.25@gmail.com>
2025-09-02 22:12:56 +0900

    楽天アフィリエイト追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,9 +65,7 @@ def play_baccarat(bet_side, bet_amount):
 # Streamlit UI
 st.markdown(
     """
-    <a href="https://af.moshimo.com/af/c/click?a_id=5158575&p_id=54&pc_id=54&pl_id=620" target="_blank">
-        <img src="https://image.moshimo.com/af-img/0032/000000000620.gif" width="468" height="60" style="border:none;">
-    </a>
+    <a href="//af.moshimo.com/af/c/click?a_id=5158607&p_id=7066&pc_id=20221&pl_id=89317" rel="nofollow" referrerpolicy="no-referrer-when-downgrade" attributionsrc><img src="//image.moshimo.com/af-img/6831/000000089317.jpg" width="468" height="60" style="border:none;"></a><img src="//i.moshimo.com/af/i/impression?a_id=5158607&p_id=7066&pc_id=20221&pl_id=89317" width="1" height="1" style="border:none;" loading="lazy">
     """,
     unsafe_allow_html=True
 )
